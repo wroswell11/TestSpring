95aa8163808388a4b7084230ec1aee8be8218685
wroswell11 <wroswell11@gmail.com>
2021-06-26 23:15:12 -0700

    Initial commit

---
+++ src/main/java/testspring/TestSpring.java
@@ -0,0 +1,11 @@
+package testspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring {
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpring.class, args);
+    }
+}
---
+++ src/main/java/testspring/TestSpringController.java
@@ -0,0 +1,16 @@
+package testspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestSpringController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String login() {
+        return "Hello World";
+    }
+}
